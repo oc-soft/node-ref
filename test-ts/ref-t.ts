1d274e62d563c97f01073727a5800ef7095a5374
Toshiyuki Ogawa <toshi@oc-soft.net>
2024-12-14 15:41:45 +0900

    I had a test script for typescript.

---
+++ test-ts/ref-t.ts
@@ -0,0 +1,17 @@
+import * as ref from '../'
+import * as assert from 'node:assert'
+import { Buffer } from 'node:buffer'
+
+function checkTypes() {
+  assert.equal(typeof ref.types.int.size, 'number', 'type.int has size') 
+}
+
+function checkFunctions()  {
+  const strBuf = ref.allocCString('Hello world')
+
+  assert.ok(strBuf instanceof Buffer, 'allocCString return Buffer instance')
+
+}
+
+
+// vi: se ts=2 sw=2 et:
